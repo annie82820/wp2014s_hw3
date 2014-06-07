5dae5fc4e08e5a81777b2ea20df5b021b71b9190
annie82820 <100403005@nccu.edu.tw>
2014-06-07 21:45:53 +0800

    new

--- js/app.js
+++ js/app.js
@@ -2,191 +2,98 @@
 	//Parse initialization
 	Parse.initialize("3Q0fl5PFmawluhNowBEogxyOlhLFIp2YTS5s5TBy", "HWWfH7Aa4E0CXz5IS0WAlQxCUMVVlbkdiEBQL7lV");
 	
-	//Compile template engine function
-	var compiled = {};
-	var views = ["loginView", "evaluationView", "updateSuccessView"];
-	for(var i = 0;i < views.length;i++){
-			var view_text = document.getElementById(views[i]).text;
-			compiled[views[i]] = doT.template(view_text);
-	}
-
-	//Shared function
-			
-	var handler = {
-		navbar: function(){
-			var users = Parse.User.current();
-			var loginButton = $("#loginButton");
-			var evaluationButton = $("#evaluationButton");
-			var logoutButton = $("#logoutButton");
-			if(users==null){
-				loginButton.css("display", "block");
-				evaluationButton.css("display", "none");
-				logoutButton.css("display", "none");
+var e={};
+	["loginView","evaluationView","updateSuccessView"].forEach(function(t){
+		templateCode=document.getElementById(t).text;e[t]=doT.template(templateCode)
+	});
+	var t={
+		loginRequiredView:function(e){
+			return function(){
+				var t=Parse.User.current();
+				if(t){
+					e()
+				}else{
+					window.location.hash="login/"+window.location.hash
+				}
 			}
-			else{
-				loginButton.css("display", "none");
-				evaluationButton.css("display", "block");
-				logoutButton.css("display", "block");
-			}
-			
-			logoutButton.click(function(){
-				Parse.User.logOut();
-				loginButton.css("display", "block");
-				evaluationButton.css("display", "none");
-				logoutButton.css("display", "none");
-				window.location = "?#login/";
-			});
-		},
-		loginView: function(){
-			//Show the login content on browser
-			var content = document.getElementById("content");
-			content.innerHTML = compiled["loginView"]();
-			
-			//check if student ID has been entered in login form
-			document.getElementById("form-signin-student-id").addEventListener("keyup", function(){
-				var loginStudentID = this.value;
-				if(TAHelp.getMemberlistOf(loginStudentID)==false){
-					document.getElementById("form-signin-message").style.display = "block";
-					document.getElementById("form-signin-message").innerHTML = "<p>The student is not one of the class students.</p>"
-				}
-				else{
-					document.getElementById("form-signin-message").style.display = "none";
-				}
-			});
-			
-			//check if student ID has been entered in sign up form
-			document.getElementById("form-signup-student-id").addEventListener("keyup", function(){
-				var signupStudentID = this.value;
-				if(TAHelp.getMemberlistOf(signupStudentID)==false){
-					document.getElementById("form-signup-message").style.display = "block";
-					document.getElementById("form-signup-message").innerHTML = "<p>The student is not one of the class students.</p>"
-				}
-				else{
-					document.getElementById("form-signup-message").style.display = "none";
-				}
-			});
-			
-			//check password correctness in sign up form
-			document.getElementById("form-signup-password1").addEventListener("keyup", function(){
-				var signupPwd = document.getElementById("form-signup-password").value;
-				var signupPwdRepeat = this.value;
-				if(signupPwd != signupPwdRepeat){
-					document.getElementById("form-signup-message").style.display = "block";
-					document.getElementById("form-signup-message").innerHTML = "<p>Passwords don't match.</p>"
-				}
-				else{
-					document.getElementById("form-signup-message").style.display = "none";
-				}	
-			});
-			
-			//check if student ID and password is correct in log in form
-			document.getElementById('form-signin').addEventListener('submit', function(){
-				Parse.User.logIn(
-					document.getElementById('form-signin-student-id').value,
-					document.getElementById('form-signin-password').value, 
-					{
-						success: function(user) {
-							handler.navbar();
-							window.location = "?#peer-evaluation/";
-						}, 
-						error: function(user, error) {
-							alert("Error:" + error.code + " " + error.message);
-							window.location = "?#login/";
-						}
-					}
-				);
-			});
-			
-			//check if student ID and password is correct in sign up form
-			document.getElementById('form-signup').addEventListener('submit', function(){
-				var user = new Parse.User();
-				user.set("username", document.getElementById("form-signup-student-id").value);
-				user.set("password", document.getElementById("form-signup-password").value);
-				user.set("email", document.getElementById("form-signup-email").value);
-				
-				user.signUp(null, {
-					success: function(user){
-						handler.navbar();
-						window.location = "?#peer-evaluation/";
-					},
-					error: function(user, error){
-						alert("Error:" + error.code + " " + error.message);
-						window.location = "?#login/";
-					}
-				});
-			});
-		},
-		evaluationView: function(){
-			var evaluation = Parse.Object.extend("Evaluation");
-			var query = new Parse.Query(evaluation);
-			query.first({
-				success: function(data){
-					var current_user = Parse.User.current();
-					var member = TAHelp.getMemberlistOf(current_user.getUsername());
-						
-					for(var i = 0;i < member.length;i++){
-						if(data === undefined){
-							member[i]["scores"] = new Array("0", "0", "0", "0");
-						}
-						else{
-							member = data.get("evaluation").slice(0);
-							break;
-						}
-						if(member[i].StudentId===current_user.getUsername()){
-							member.splice(i, 1);
-							i--;
-						}
-						
-					}
-					
-					document.getElementById("content").innerHTML = (compiled.evaluationView(member));
-					for(var i = 0;i < 4;i++){
-						for(var j = 0;j < 3;j++){
-							$("stu"+member.StudentId+"-q"+j.toString()).val(member[i].scores[j]);
-						}
-					}
-					document.getElementById('evaluationForm').addEventListener('submit', function(){
-						for(var i = 0;i < member.length;i++){
-							var total = 0;
-							for(var j = 0;j < 4; j++){
-								var tmp_score = $("#stu"+member[i]["StudentId"]+"-q"+j.toString()).val();
-								member[i]["scores"][j] = tmp_score; 
-							}
-						}
-						var changed = new evaluation();
-						changed.set("user",Parse.User.current());
-						changed.set("evaluation",data.get("evaluation").slice(0));
-						if(originData === undefined){
-							changed.save(null, {
-								success: function(changed){
-									console.log("New row added!!");
-									document.getElementById("content").innerHTML = (compiled.updateSuccessView());
-								},
-								error: function(changed, error){
-									alert("Error:" + error.code + " " + error.message);
-								}
-							});
-						}
-						else{
-							changed.save(null, {
-								success: function(changed){
-									changed.set("evaluation", member);
-									changed.save();
-									console.log("Data changed.");
-									document.getElementById("content").innerHTML = (compiled.updateSuccessView());
-								}
-							});
-						}	
-					});
-				},
-				error: function(data, error){
-					alert("Error:" + error.code + " " + error.message);
-				}
-			});
 		}
 	};
-	
-var s=function(){
+	var n={
+		navbar:function(){
+			var e=Parse.User.current();
+			if(e){
+				document.getElementById("loginButton").style.display="none";
+				document.getElementById("logoutButton").style.display="block";
+				document.getElementById("evaluationButton").style.display="block"
+			}else{
+				document.getElementById("loginButton").style.display="block";
+				document.getElementById("logoutButton").style.display="none";
+				document.getElementById("evaluationButton").style.display="none"
+			}
+			document.getElementById("logoutButton").addEventListener("click",function(){
+				Parse.User.logOut();
+				n.navbar();
+				window.location.hash="login/"
+			})
+		},
+		evaluationView:t.loginRequiredView(function(){
+			var t=Parse.Object.extend("Evaluation");
+			var n=Parse.User.current();
+			var r=new Parse.ACL;
+			r.setPublicReadAccess(false);
+			r.setPublicWriteAccess(false);
+			r.setReadAccess(n,true);
+			r.setWriteAccess(n,true);
+			var i=new Parse.Query(t);
+			i.equalTo("user",n);
+			i.first(
+				{success:function(i){
+					window.EVAL=i;
+					if(i===undefined){
+						var s=TAHelp.getMemberlistOf(n.get("username")).filter(function(e){
+							return e.StudentId!==n.get("username")?true:false
+						}).map(function(e){
+							e.scores=["0","0","0","0"];
+							return e})
+					}else{
+						var s=i.toJSON().evaluations
+					}
+					document.getElementById("content").innerHTML=e.evaluationView(s);
+					document.getElementById("evaluationForm-submit").value=i===undefined?"送出表單":"修改表單";
+					document.getElementById("evaluationForm").addEventListener("submit",function(){
+						for(var o=0;o<s.length;o++){
+							for(var u=0;u<s[o].scores.length;u++){
+								var a=document.getElementById("stu"+s[o].StudentId+"-q"+u);
+								var f=a.options[a.selectedIndex].value;
+								s[o].scores[u]=f
+							}
+						}
+						if(i===undefined){
+							i=new t;
+							i.set("user",n);
+							i.setACL(r)
+						}
+						console.log(s);
+						i.set("evaluations",s);
+						i.save(null,
+							{success:function(){
+								document.getElementById("content").innerHTML=e.updateSuccessView()
+							},error:function(){}})},false)
+				},
+				error:function(e,t){}})
+		}),
+		loginView:function(t){
+			var r=function(e){
+				var t=document.getElementById(e).value;
+				return TAHelp.getMemberlistOf(t)===false?false:true};
+				var i=function(e,t,n){if(!t()){
+					document.getElementById(e).innerHTML=n;
+					document.getElementById(e).style.display="block"
+				}else{
+					document.getElementById(e).style.display="none"
+				}
+		};
+	var s=function(){
 		n.navbar();
 		window.location.hash=t?t:""
 	};
